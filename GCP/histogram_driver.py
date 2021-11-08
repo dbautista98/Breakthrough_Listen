480dd6fc5bc94ccd4a7269edf8f2d09c08e48bd7
Daniel <dbautista98@berkeley.edu>
2021-11-08 04:27:33 +0000

    quick fix

--- GCP/histogram_driver.py
+++ GCP/histogram_driver.py
@@ -9,7 +9,7 @@ thresholds = 2**np.arange(13)
 for band in bands:
     for threshold in thresholds:
         lowercase = band.lower()
-        output = "python3 /home/dbautista98/Brekthrough_Listen/GCP/compute_hist.py %s /home/dbautista98/energy-detection/%s-band/ -t %s -o histograms/%s-band/ -nf"%(band, lowercase, threshold, lowercase)
+        output = "python3 /home/dbautista98/Breakthrough_Listen/GCP/compute_hist.py %s /home/dbautista98/energy-detection/%s-band/ -t %s -o histograms/%s-band/ -nf"%(band, lowercase, threshold, lowercase)
         print(output)
         os.system(output)
 

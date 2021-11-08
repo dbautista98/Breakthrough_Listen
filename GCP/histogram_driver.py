4b5794144428a48057d141c45ae63b0d700bd77f
Daniel <dbautista98@berkeley.edu>
2021-11-07 20:24:47 -0800

    remove notch filter

--- GCP/histogram_driver.py
+++ GCP/histogram_driver.py
@@ -9,7 +9,7 @@ thresholds = 2**np.arange(13)
 for band in bands:
     for threshold in thresholds:
         lowercase = band.lower()
-        output = "python3 compute_hist.py %s /home/dbautista98/energy-detection/%s-band/ -t %s -o histograms/%s-band/"%(band, lowercase, threshold, lowercase)
+        output = "python3 /home/dbautista98/Brekthrough_Listen/GCP/compute_hist.py %s /home/dbautista98/energy-detection/%s-band/ -t %s -o histograms/%s-band/ -nf"%(band, lowercase, threshold, lowercase)
         print(output)
         os.system(output)
 

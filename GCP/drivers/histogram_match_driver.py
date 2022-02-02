7a7ea8790bf4b4ee62b9ba95adb473e63b48ad2e
Daniel <dbautista98@berkeley.edu>
2022-02-02 15:12:54 -0800

    added histogram_match_driver.py

---
+++ GCP/drivers/histogram_match_driver.py
@@ -0,0 +1,14 @@
+import os
+import numpy as np
+
+
+
+bands = ["L", "S", "X", "C"]
+
+for band in bands:
+    lowercase = band.lower()
+    output = "python3 /home/dbautista98/Breakthrough_Listen/GCP/histogram_match.py %s /home/dbautista98/energy-detection/%s-band/ /home/dbautista98/histograms/turboSETI/%s_band_turboSETI_hist.csv -o /home/dbautista98/histograms/threshold_comparisons -nf"%(band, lowercase, band)
+    print(output)
+    os.system(output)
+
+print("ALL DONE")

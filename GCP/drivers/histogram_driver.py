81324bec93b93a1701186b49bf104b09245a21e1
Daniel <dbautista98@berkeley.edu>
2022-02-07 15:46:58 -0800

    add drop nodes option

--- GCP/drivers/histogram_driver.py
+++ GCP/drivers/histogram_driver.py
@@ -5,11 +5,12 @@ import numpy as np
 
 bands = ["L", "S", "X", "C"]
 thresholds = [4096]#2**np.arange(16)
+dropped_csv = "/home/dbautista98/dropped_nodes.csv"
 
 for band in bands:
     for threshold in thresholds:
         lowercase = band.lower()
-        output = "python3 /home/dbautista98/Breakthrough_Listen/GCP/compute_hist.py %s /home/dbautista98/energy-detection/%s-band/ -t %s -o /home/dbautista98/histograms/energy_detection_csvs/ -nf -s"%(band, lowercase, threshold)
+        output = "python3 /home/dbautista98/Breakthrough_Listen/GCP/compute_hist.py %s /home/dbautista98/energy-detection/%s-band/ -t %s -o /home/dbautista98/histograms/energy_detection_csvs/ -nf -s -c %s"%(band, lowercase, threshold, dropped_csv)
         print(output)
         os.system(output)
 

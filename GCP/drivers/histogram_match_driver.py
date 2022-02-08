3f5993df63e3fa00dc6e33c756c28c95abbb0609
Daniel <dbautista98@berkeley.edu>
2022-02-07 18:44:03 -0800

    update to allow for dropped node checks

--- GCP/drivers/histogram_match_driver.py
+++ GCP/drivers/histogram_match_driver.py
@@ -7,7 +7,7 @@ bands = ["L", "S"]
 
 for band in bands:
     lowercase = band.lower()
-    output = "python3 /home/dbautista98/Breakthrough_Listen/GCP/histogram_match.py %s /home/dbautista98/energy-detection/%s-band/ /home/dbautista98/histograms/turboSETI/%s_band_turboSETI_hist.csv -o /home/dbautista98/histograms/threshold_comparisons -nf -p"%(band, lowercase, band)
+    output = "python3 /home/dbautista98/Breakthrough_Listen/GCP/histogram_match.py %s /home/dbautista98/energy-detection/%s-band/ /home/dbautista98/histograms/turboSETI/%s_band_turboSETI_hist.csv -o /home/dbautista98/histograms/threshold_comparisons -nf -s -p -c /home/dbautista98/dropped_nodes.csv"%(band, lowercase, band)
     print(output)
     os.system(output)
 

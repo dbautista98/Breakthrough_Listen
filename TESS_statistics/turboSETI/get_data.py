e60f3e829bf8d36e72821e2a0ccf3e2d2ad70be4
Daniel <dbautista98@berkeley.edu>
2022-05-12 16:17:37 -0700

    added get_data.py

---
+++ TESS_statistics/turboSETI/get_data.py
@@ -0,0 +1,15 @@
+import glob
+import pandas as pd
+import os 
+
+def get_data():
+    current_dir = __file__.replace(os.path.basename(__file__), "")
+    csvs = glob.glob(current_dir + "*csv")
+    csvs.sort()
+
+    L = pd.read_csv(csvs[1])
+    S = pd.read_csv(csvs[2])
+    C = pd.read_csv(csvs[0])
+    X = pd.read_csv(csvs[3])
+
+    return {"L":L, "S":S, "C":C, "X":X}
